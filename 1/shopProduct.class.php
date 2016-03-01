ec5aafb5c28b20ddd021f798e4f8b3e692a89711
hellomoto177 <hellomoto177@yandex.ru>
2016-03-01 17:22:37 +0500

    step 1

--- 1/shopProduct.class.php
+++ 1/shopProduct.class.php
@@ -3,15 +3,65 @@ class ShopProduct {
 	public $title = '';
 	public $producer = '';
 	public $price = '';
-	public $numpages = '';
+	public $numPages = '';
+	public $playLength = '';
+	public $summartLine = '';
+
+	public function getSummaryLine() {
+		if($this instanceOf Book) {
+			$title = $this->title;
+			$producer = $this->producer;
+			$numPages = $this->numPages;
+			print("Название: $title\nСоздатель: $producer\nКоличество страниц: $numPages\n");
+		}
+
+		elseif($this instanceOf Cd) {
+			$title = $this->title;
+			$producer = $this->producer;
+			$playLength = $this->playLength;
+			print("Название: $title\nСоздатель: $producer\nПродолжительность: $playLength\n");
+		}
+		else
+			print('Error');
+	}
 }
 
-$product = new ShopProduct();
+class ShopProductWriter {
+	public function write(ShopProduct $product) {
+		print("Писатель: $product->producer\n");
+	}
+}
 
-$product->title = 'Мастер и Маргарита';
-$product->producer = 'Булгаков';
-$product->price = '99';
-$product->numpages = '400';
+class Book extends ShopProduct {
+	public function __construct($title, $producer, $price, $numPages) {
+		$this->title = $title;
+		$this->producer = $producer;
+		$this->price = $price;
+		$this->numPages = $numPages;
+	}
+}
 
-var_dump($product);
+class Cd extends ShopProduct {
+	public function __construct($title, $producer, $price, $playLength) {
+		$this->title = $title;
+		$this->producer = $producer;
+		$this->price = $price;
+		$this->playLength = $playLength;
+	}
+}
+
+$book = new Book('Мастер и Маргарита',
+			   'Булгаков',
+			   '499',
+			   '350');
+
+$cd = new Cd('Звездные войны',
+			   'Джордж Лукас',
+			   '2199',
+			   '90');
+
+// $book->getSummaryLine();
+$cd->getSummaryLine();
+// $writer = new ShopProductWriter();
+// $writer->write($product);
 ?>
